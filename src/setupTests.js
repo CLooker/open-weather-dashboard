0a0ed0a06900b3303492776c46c2bbd81e0f166c
CLooker <chad.looker@gmail.com>
2018-12-05 23:51:37 -0500

    fix tests that no longer run

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
+global.fetch = require('jest-fetch-mock');
 import requestAnimationFrame from './tempPolyfills';
- 
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
- 
+
 configure({ adapter: new Adapter(), disableLifecycleMethods: true });
