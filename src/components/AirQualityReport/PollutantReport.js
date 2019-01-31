0417f0b12ac200aa9dc1fed4d7f5d6676ddb6d50
CLooker <chad.looker@gmail.com>
2019-01-30 22:46:08 -0500

    Use PureComponent to stop needless re-rendering

--- src/components/AirQualityReport/PollutantReport.js
+++ src/components/AirQualityReport/PollutantReport.js
@@ -1,62 +1,65 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const PollutantReport = ({ pollutant, data }) => {
-  const { value, pressure, precision } = data;
+export default class PollutantReport extends PureComponent {
+  static propTypes = {
+    pollutant: PropTypes.string,
+    data: PropTypes.object
+  };
 
-  return (
-    <div
-      style={{
-        margin: '0 5px'
-      }}
-    >
-      <strong title='Pollutant'>{pollutant.toUpperCase()}</strong>
+  render() {
+    const {
+      pollutant,
+      data: { value, pressure, precision }
+    } = this.props;
+
+    return (
       <div
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          height: '15px',
-          margin: '2px 0'
+          margin: '0 5px'
         }}
       >
-        <h5 style={{ padding: '0 2px 0 0' }} title='Mixing Ratio'>
-          Value:
-        </h5>
-        {value}
+        <strong title='Pollutant'>{pollutant.toUpperCase()}</strong>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '15px',
+            margin: '2px 0'
+          }}
+        >
+          <h5 style={{ padding: '0 2px 0 0' }} title='Mixing Ratio'>
+            Value:
+          </h5>
+          {value}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '15px',
+            margin: '2px 0'
+          }}
+        >
+          <h5 style={{ padding: '0 2px 0 0' }} title='Atmospheric Pressure'>
+            Pressure:
+          </h5>
+          {pressure} hPa
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            height: '15px',
+            margin: '2px 0'
+          }}
+        >
+          <h5 style={{ padding: '0 2px 0 0' }} title='Measurement Precision'>
+            Precision:
+          </h5>
+          {precision}
+        </div>
       </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          height: '15px',
-          margin: '2px 0'
-        }}
-      >
-        <h5 style={{ padding: '0 2px 0 0' }} title='Atmospheric Pressure'>
-          Pressure:
-        </h5>
-        {pressure} hPa
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          height: '15px',
-          margin: '2px 0'
-        }}
-      >
-        <h5 style={{ padding: '0 2px 0 0' }} title='Measurement Precision'>
-          Precision:
-        </h5>
-        {precision}
-      </div>
-    </div>
-  );
-};
-
-PollutantReport.propTypes = {
-  pollutant: PropTypes.string,
-  data: PropTypes.object
-};
-
-export default PollutantReport;
+    );
+  }
+}
