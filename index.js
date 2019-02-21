a7545e294549736daadfd8e37559b3e7d339964c
CLooker <chad.looker@gmail.com>
2019-02-20 20:48:16 -0500

    Update for deploy

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 express()
-  .use((req, res, next) => {
-    req.url = req.url.replace('/open-weather-dashboard/', '/');
-    next();
-  })
   .use(express.static(path.join(__dirname, 'client/build')))
   .listen(PORT, () => console.log(`server is listening on port ${PORT}`));
